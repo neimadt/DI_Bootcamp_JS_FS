5c84776498cb381389d0f0f7ebabbf0bd2c7c44a
neimadt <damtoulet@yahoo.fr>
2023-02-23 20:59:07 +0400

    miscs

---
+++ week3/day1/examples/ex1/main.js
@@ -0,0 +1,15 @@
+let n1 = 1, n2 = 2;
+let s1 = '1', s2 = '2';
+
+console.log(n1 == s1)
+console.log(n1 === s1)
+console.log(n1 != n2)
+console.log(n1 != s1)
+console.log(n1 !== s1)
+
+
+console.log(n1 <= n2)
+
+let b1 = true, b2 = false;
+console.log(b1 && b2)
+console.log(b1 || b2)
